c415ac365716fc752484c7a63b3994ab6f4117a1
Stefan Frerix <stefan.frerix@gmail.com>
2017-12-08 15:06:20 +0100

    Added Database support - once more

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -23,13 +23,10 @@ public class InsultGenerator {
             if (connection != null) {
                 String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , " +
                         "long_adjective b, noun c ORDER BY random() limit 1";
-
-
                 Statement stmt = connection.createStatement();
                 ResultSet rs = stmt.executeQuery(SQL);
-
                 while (rs.next()) {
-                    if (vowels.indexOf(rs.getString("first").charAt(0)) == 1) {
+                    if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
                         article = "a";
                     }
                     theInsult = String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
@@ -37,7 +34,6 @@ public class InsultGenerator {
                 }
                 rs.close();
                 connection.close();
-
             }
         } catch (Exception e) {
             return "Database connection problem!";
